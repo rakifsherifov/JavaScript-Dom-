0384e6857a71ce17e7928f761db5b3cfc7bad447
rakifsherifov <rakifrsh@code.edu.az>
2023-04-09 23:04:04 +0400

    elave edildi

---
+++ script.js
@@ -0,0 +1,53 @@
+let responsiveSlider = function() {
+    let slider = document.getElementById("slider");
+    let sliderWidth = slider.offsetWidth;
+    let slideList = document.getElementById("slideWrap");   
+    let count = 1;
+    let items = slideList.querySelectorAll("li").length;
+    let prev = document.getElementById("prev");
+    let next = document.getElementById("next");
+
+
+    window.addEventListener('resize', function() {
+        sliderWidth = slider.offsetWidth;
+    });
+
+    let prevSlide = function() {
+        if(count > 1) {
+            count = count - 2;
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++;
+        }
+        else if(count = 1) {
+            count = items - 1;
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++; 
+        }
+    };
+
+    let nextSlide = function() {
+        if(count < items) {
+            slideList.style.left = "-" + count * sliderWidth + "px";
+            count++;
+        }
+        else if(count = items) {
+            slideList.style.left = "0px";
+            count = 1; 
+        }
+    };
+
+    next.addEventListener("click", function() {
+        nextSlide();
+    });
+    prev.addEventListener("click", function() {
+        prevSlide();
+    });
+
+    setInterval(function() {
+        nextSlide()
+    }, 5000);
+};
+
+window.onload = function() {
+    responsiveSlider();
+}
